8935507382c3fec458490bcc881f8ba40d74b6d0
yazid mekhtoub <mekhy593@newschool.edu>
2020-05-20 14:30:30 -0400

    Updates

---
+++ precache-manifest.00d2fc76632ebd69c1e19682ed430f42.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38cd8031934b197ba685dc550c74044a",
+    "url": "/github.io/ProjectTwo/index.html"
+  },
+  {
+    "revision": "ae89febb78b11640da5c",
+    "url": "/github.io/ProjectTwo/static/css/main.0eff1277.chunk.css"
+  },
+  {
+    "revision": "f7dd00225ca4b4b25eec",
+    "url": "/github.io/ProjectTwo/static/js/2.fd23b5ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/ProjectTwo/static/js/2.fd23b5ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae89febb78b11640da5c",
+    "url": "/github.io/ProjectTwo/static/js/main.50cea31b.chunk.js"
+  },
+  {
+    "revision": "950c044f7c30d7e14820",
+    "url": "/github.io/ProjectTwo/static/js/runtime-main.1425121b.js"
+  },
+  {
+    "revision": "9911f9f4d94da32427593741f55dd7fd",
+    "url": "/github.io/ProjectTwo/static/media/baData.9911f9f4.csv"
+  },
+  {
+    "revision": "be06f672257047214e17f4a1a9111bf7",
+    "url": "/github.io/ProjectTwo/static/media/stackData.be06f672.csv"
+  }
+]);
