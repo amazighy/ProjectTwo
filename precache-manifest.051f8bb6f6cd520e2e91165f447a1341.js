28aa4abd99e52d0f627d549371e855ebc74c4539
yazid mekhtoub <mekhy593@newschool.edu>
2020-05-20 14:49:19 -0400

    Updates

---
+++ precache-manifest.051f8bb6f6cd520e2e91165f447a1341.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c26ac5b4529c5891bb61f72486ce4f2d",
+    "url": "/ProjectTwo/index.html"
+  },
+  {
+    "revision": "ecbae16aa1a5d80dcab1",
+    "url": "/ProjectTwo/static/css/main.0eff1277.chunk.css"
+  },
+  {
+    "revision": "f7dd00225ca4b4b25eec",
+    "url": "/ProjectTwo/static/js/2.fd23b5ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ProjectTwo/static/js/2.fd23b5ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecbae16aa1a5d80dcab1",
+    "url": "/ProjectTwo/static/js/main.cf5f9a7d.chunk.js"
+  },
+  {
+    "revision": "083f1704c11aec3ddc7b",
+    "url": "/ProjectTwo/static/js/runtime-main.97779888.js"
+  },
+  {
+    "revision": "9911f9f4d94da32427593741f55dd7fd",
+    "url": "/ProjectTwo/static/media/baData.9911f9f4.csv"
+  },
+  {
+    "revision": "be06f672257047214e17f4a1a9111bf7",
+    "url": "/ProjectTwo/static/media/stackData.be06f672.csv"
+  }
+]);
